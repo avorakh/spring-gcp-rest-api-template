cbe801a45f60c6a0b437b079b54a95af66b5c724
Aliaksandr Vorakh <39852199+avorakh@users.noreply.github.com>
2025-11-18 01:22:59 +0100

    feature(ISDP-4): add spring gcp starter (#2)

--- src/test/java/dev/avorakh/gcp/template/SpringRestApiTemplateApplicationTests.java
+++ src/test/java/dev/avorakh/gcp/template/SpringRestApiTemplateApplicationTests.java
@@ -1,14 +1,27 @@
 package dev.avorakh.gcp.template;
 
+import com.google.api.gax.core.CredentialsProvider;
+import com.google.cloud.spring.core.GcpProjectIdProvider;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class SpringRestApiTemplateApplicationTests {
 
+    @Autowired
+    private CredentialsProvider googleCredentials;
+
+    @Autowired
+    private GcpProjectIdProvider gcpProjectIdProvider;
+
     @Test
     void contextLoads() {
         // context load check
+        assertNotNull(googleCredentials, "googleCredentials bean should be present");
+        assertNotNull(gcpProjectIdProvider, "gcpProjectIdProvider bean should be present");
     }
 
 }
